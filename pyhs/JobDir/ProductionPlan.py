fea3610ec32a285ced50b25ab0bc0766d862c5df
zeric <zeric0212@hotmail.com>
2022-07-31 00:53:39 +0800

    introduce _find() for other functions to operate; making ppbook and ppsheet class attr

--- pyhs/JobDir/ProductionPlan.py
+++ pyhs/JobDir/ProductionPlan.py
@@ -1,7 +1,28 @@
 import gspread
 
 class ProductionPlan:
+
     gc = gspread.service_account(filename='Resources/Credentials/service_account.json')
-    def __init__(self):
-        self.workbook = ProductionPlan.gc.open('2022 HK Production Planning')
-        self.prodPlan = self.workbook.worksheet('2022')
+    ppbook = gc.open('Copy of 2022 HK Production Planning')
+    ppsheet = ppbook.worksheet('2022')
+
+    def __init__(self, job_name):
+        self.jobCell = ProductionPlan.ppsheet.find(job_name)
+
+    def _find(self, keyword):
+        colToFind = ProductionPlan.ppsheet.find(keyword)
+        return ProductionPlan.ppsheet.cell(self.jobCell.row, colToFind.col)
+
+    def get_vendor(self):
+        return self._find('Vendor').value
+
+    def get_job_status(self):
+        return self._find('Job Status').value
+
+    def check_download(self):
+        if self._find('Job Downloaded').value == 'TRUE':
+            return True
+        return False
+
+    def update_job_status(self):
+        pass
