0f0e50cc3d572bb007a5e951e7c2d6abf4125eec
zeric <zeric0212@hotmail.com>
2022-08-04 00:53:54 +0800

    introduce check_img_name() & Img._access_json() for both check functions

--- pyhs/Img.py
+++ pyhs/Img.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import sys
+import re
 import json
 
 class Img:
@@ -27,22 +29,40 @@ class Img:
         self.imgNum = len(self.get_img_list())
         return self.imgNum
 
+    @classmethod
+    def _access_json(cls, brand):
+        with open('Resources/BrandDatabase.json') as brandJsonFile:
+            Img.brandJson = json.load(brandJsonFile)
+            return Img.brandJson[brand]
+
     def check_img_spec(self, brand):
-        Img.brandJsonFile = open('Resources/BrandDatabase.json')
-        Img.brandJson = json.load(Img.brandJsonFile)
-        Img.brandData = Img.brandJson[brand]['Spec']
+        Img.brandImgSpec = Img._access_json(brand)['Spec']
+        self.wrongSpecList = [] #change to dictionary with key as img, value as specs
 
         for img in self.imgPathList:
-            imgObj = Image.open(self.imgDir / img)
+            with Image.open(self.imgDir / img) as imgObj:
+                for spec, value in Img.brandImgSpec.items():
+                    if str(eval(f'imgObj.{spec}')) != value:
+                        self.wrongSpecList.append(img)
+                        # wrongSpec = str(eval(f'imgObj.{spec}'))
+                        # print(f'Error: Wrong Image Spec\nimg: {img}\ncorrect spec: {value}\nwrong spec: {wrongSpec}')
 
-            for spec, value in Img.brandData.items():
-                if str(eval(f'imgObj.{spec}')) != value:
-                    wrongSpec = str(eval(f'imgObj.{spec}'))
-                    print(f'Error: Wrong Image Spec\nimg: {img}\ncorrect spec: {value}\nwrong spec: {wrongSpec}')
-                    return False
-            
-            imgObj.close()
+        print('All Image Spec Checked')
+        if len(self.wrongSpecList) != 0:
+            print(self.wrongSpecList)
+            sys.exit(1)
 
-        Img.brandJsonFile.close()
-        print('All Images Checked')
-        return True
+    def check_img_name(self, brand):
+        Img.brandCorName = Img._access_json(brand)['Name']
+        corName = re.compile(r'{}'.format(Img.brandCorName))
+        self.wrongNameList = []
+
+        for img in self.imgNameList:
+                if not corName.search(img):
+                    print(f'Error: Wrong Image Name\nimg: {img}')
+                    self.wrongNameList.append(img)
+
+        print('All Image Name Checked')
+        if len(self.wrongNameList) != 0:
+            print(self.wrongNameList)
+            sys.exit(1)
