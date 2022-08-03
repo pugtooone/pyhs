7c8c3c254cbf323f8c55d9dae04ba53754f98131
zeric <zeric0212@hotmail.com>
2022-08-03 18:54:20 +0800

    split imgList into imgNameList & imgPathList, for check_img_name() & check_img_spec() respectively; glob recursively

--- pyhs/Img.py
+++ pyhs/Img.py
@@ -1,6 +1,5 @@
 from PIL import Image
-from pathlib import Path
-import sys, json
+import json
 
 class Img:
     def __init__(self, directory):
@@ -9,14 +8,23 @@ class Img:
         Parameter: Path obj of JobDir
         """
         self.imgDir = directory / 'Images'
-        self.imgListIter = self.imgDir.glob('*.tif') #Iterator for img
+        self.imgListIter = self.imgDir.glob('**/*.tif') #Iterator for img
+
+        self.imgNameList = []
+        for i in self.imgListIter:
+           img = i.name
+           self.imgNameList.append(img)
+
+        self.imgPathList = []
+        for i in self.imgListIter:
+           img = i
+           self.imgNameList.append(img)
 
     def get_img_list(self):
-        self.imgList = list(self.imgListIter)
-        return self.imgList
+        return self.imgNameList
 
     def get_img_num(self):
-        self.imgNum = len(self.imgList)
+        self.imgNum = len(self.get_img_list())
         return self.imgNum
 
     def check_img_spec(self, brand):
@@ -24,8 +32,8 @@ class Img:
         Img.brandJson = json.load(Img.brandJsonFile)
         Img.brandData = Img.brandJson[brand]['Spec']
 
-        for img in self.imgListIter:
-            imgObj = Image.open(img)
+        for img in self.imgPathList:
+            imgObj = Image.open(self.imgDir / img)
 
             for spec, value in Img.brandData.items():
                 if str(eval(f'imgObj.{spec}')) != value:
