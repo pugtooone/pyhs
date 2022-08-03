b7ef7a81a058a8f8ef90e72b983d745d49c7998d
zeric <zeric0212@hotmail.com>
2022-08-03 18:56:13 +0800

    introduce CLI Menu module

---
+++ pyhs/Menu.py
@@ -0,0 +1,69 @@
+from JobDir import ToSend
+from pathlib import Path
+from tkinter.filedialog import askdirectory
+from AddOns import pdf_extract
+import sys
+
+class Menu:
+    def __init__(self):
+        self.choice={
+                     '1': 'self.ToSend',
+                     '2': 'self.QC',
+                     '3': 'self.Submit',
+                     '4': 'self.AddOn',
+                     '5': 'self.Quit'
+                     }
+
+    def display_menu(self):
+        print('''
+        pyhs Menu
+
+        [1]: To Send
+        [2]: QC
+        [3]: Submit
+        [4]: Add On Functions
+        [5]: Quit
+                ''')
+
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input('Enter your option: \n')
+            action = self.choice.get(choice)
+            if action:
+                eval(f'{action}()')
+            else:
+                print('Error: Invalid Option')
+
+    #Main flow
+    def ToSend(self):
+        jobFolder = Path(askdirectory())
+        toSendFolder = ToSend(jobFolder)
+        toSendFolder.check_img_spec()
+        toSendFolder.write_email()
+
+    def AddOn(self):
+        self.addOnChoice={
+                     '1': 'pdf_extract.main',
+                     '2': 'back'
+                     }
+        while True:
+            print("""
+            Add Ons
+            [1]: Extract PDF
+            [2]: Back
+                    """)
+            choice = input('Enter your option: ')
+            action = self.addOnChoice.get(choice)
+            if choice == '2':
+                break
+            elif action:
+                eval(f'{action}()')
+            else:
+                print('Error: Invalid Option')
+
+    def Quit(self):
+        sys.exit(0)
+
+if __name__ == '__main__':
+    Menu().run()
