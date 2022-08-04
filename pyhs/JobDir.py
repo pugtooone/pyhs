8675379a6c9ce5da6491bd2104b76329d21cf4ac
zeric <zeric0212@hotmail.com>
2022-08-04 19:04:27 +0800

    introduce QC subclass; move _check_dir_structure() to ToSend subclass

--- pyhs/JobDir.py
+++ pyhs/JobDir.py
@@ -8,35 +8,24 @@ import pyperclip
 
 class JobDir:
     # class attributes
-    #brandBaseDir = Path('/Volumes/Studio/CLIENTS/')
-    #brandBase = os.listdir(brandBaseDir)
+    brandBaseDir = Path('/Volumes/Studio/CLIENTS/')
+    brandBase = os.listdir(brandBaseDir)
 
     def __init__(self, directory):
         """
         initialize JobDir obj
         Parameter: Path obj of JobDir
         """
-        JobDir._check_dir_structure(directory)
         self.jobDir = directory
         self.jobName = directory.name
         self.imgDirObj = Img(self.jobDir)
         self.docDirObj = Doc(self.jobDir)
         self.prodPlanObj = ProductionPlan(self.jobName)
 
-    @classmethod
-    def _check_dir_structure(cls, directory):
-        jobDirls = os.listdir(directory)
-        if 'Images' in jobDirls and 'Documents' in jobDirls:
-            pass
-        else:
-            #could change to return boolean, then a restructure function
-            print('Error: wrong folder structure')
-            sys.exit(2)
-
     def get_brand(self):
         for brand in JobDir.brandBase:
             if brand in self.jobName:
-                return brand
+                return str(brand)
         return None
 
 
@@ -75,13 +64,30 @@ class JobDir:
 class ToSend(JobDir):
     def __init__(self, directory):
         super().__init__(directory)
+        self._check_dir_structure(directory)
+
+    def _check_dir_structure(self, directory):
+        jobDirls = os.listdir(directory)
+        if 'Images' in jobDirls and 'Documents' in jobDirls:
+            pass
+        else:
+            #could change to return boolean, then a restructure function
+            print('Error: wrong folder structure')
+            sys.exit(2)
 
     def write_email(self):
         self.imgNum = self.get_img_num()
         self.docItems = self.get_doc_items()
         self.email = f'Hi!\n\nPlease note that {self.jobName} is being uploaded to the server, including {self.imgNum} images along with {self.docItems}. Let me know if there is any question. Thanks!\n\n'
         pyperclip.copy(self.email)
-        print('email template copied')
+        print('\nEmail Template Copied')
 
     def check_img_spec(self):
         return self.imgDirObj.check_img_spec('ToSend')
+
+class QC(JobDir):
+    def __init__(self, directory):
+        super().__init__(directory)
+
+    def download_batch(self):
+        pass
