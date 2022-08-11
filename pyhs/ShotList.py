a11eab31517b4704433ef74f1a9b1364b2d94292
zeric <zeric0212@hotmail.com>
2022-08-11 19:02:50 +0800

    introduce fill_qc_tab()

--- pyhs/ShotList.py
+++ pyhs/ShotList.py
@@ -15,6 +15,17 @@ class ShotList:
             self.brandShotList = ShotList.gc.open(self.shotListName)
             self.qcTab = self.brandShotList.worksheet('For QC')
 
-    def fill_qc_tab(self):
+    def fill_qc_tab(self, imgNum, imgNameList):
+        self.qcTab.batch_clear(['A2:A'])
+        self.qcTabRange = 'A2:A' + imgNum
+
+        #convert list (row: [a,b]) into list of lists (column: [[a],[b]]) 
+        self.qcTabInput = []
+        for img in imgNameList:
+            self.qcTabInput.append([img])
+        self.qcTabDict = {'range': self.qcTabRange, 'values': self.qcTabInput}
+        #batch_update() accepts a list of dicts
+        self.qcTab.batch_update([self.qcTabDict])
+
+    def fill_shotlist(self):
         pass
-        # self.qcTab.update(len, imgNamelist)
