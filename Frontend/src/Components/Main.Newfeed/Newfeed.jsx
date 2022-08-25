613c41c3d8bbce44e8517a09769fd07d00f3ca15
BlitzQuit <102532124+BlitzQuit@users.noreply.github.com>
2022-08-26 01:09:06 +0800

    bug fixes

--- Frontend/src/Components/Main.Newfeed/Newfeed.jsx
+++ Frontend/src/Components/Main.Newfeed/Newfeed.jsx
@@ -5,9 +5,7 @@ function Newfeed(props){
     //stylesheet
     
     const expandedcontent = {height:'200px',
-                             display:'flex',
-                             flexDirection:'column',
-                             transition:'height 2s ease-out'
+                             transition:'height 0.3s ease-out'
                             };
 
     const unexpandcontent = {height:'36px',
@@ -19,9 +17,6 @@ function Newfeed(props){
 
     //Event Handler
 
-    const boxHandler = () => {
-        expand?box_style.replace(expandedcontent):box_style.replace(unexpandcontent)
-    };
 
     const expandhandler = () => {
         if (expand == false){
@@ -33,16 +28,18 @@ function Newfeed(props){
     };
 
     return(
-    <div className ="Box" onClick={expandhandler} style={expand?expandedcontent:unexpandcontent}>
-        <div onClick={boxHandler}>
+    <div className={urgent?'Urgent_Box':'Box'} style={expand?expandedcontent:unexpandcontent}>
+        <div className='Surface'>
             <div className="Circle"><box-icon name='file' color='#2B2A2E'></box-icon></div>
             <span className="Date">{props.date}</span>
-            <span className="JobName">{props.children}</span>
+            <span className="JobName" onClick={expandhandler}>{props.children}</span>
             <div className ="Start">{props.action}</div>
             <div className = "Snooze">Snooze</div>
         </div>
-        <div style={expand?{visibility:'visible',display:'relative',marginTop:'30px',opacity:'1',transition:'visibility 2s ease-out'}:{visibility:'hidden',opcaity:'0'}}>
-            <span>Image Check List</span>
+        <div style={{marginTop:'30px'}}>
+            <span>Vendor</span>
+            <span>Dresma</span>
+            
         </div>
     </div>
     );
