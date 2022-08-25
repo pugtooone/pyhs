91a226cb7e44922de1ed2f5712eca17129b3f53c
zeric <zeric0212@hotmail.com>
2022-08-26 00:09:07 +0800

    finish download_job()

--- pyhs/FileTransmitter.py
+++ pyhs/FileTransmitter.py
@@ -10,35 +10,6 @@ class FileTransmitter:
     def __init__(self, vendor):
         self.vendor = vendor
 
-    def download_job(self, job):
-        ftp = self._connect_ftp('download')
-        ftp.cwd(job)
-
-        jobPath = FileTransmitter.TBQ / job
-        jobPath.mkdir(exist_ok=True)
-
-        def ftp_loop(fileType):
-            if fileType == ['dir']:
-
-
-        while True:
-            fileList = ftp.mlsd() #return tuple (filename, data dict)
-
-            for file, data in fileList:
-                fileType = data['type']
-
-                if fileType == ['dir']:
-                    jobPath = jobPath / file
-                    jobPath.mkdir(exist_ok=True)
-                    ftp.cwd(file)
-                    continue
-                elif fileType == ['file']:
-                    filePath = jobPath / file
-                    with open(filePath, 'wb') as file:
-                        ftp.retrbinary('RETR {}'.format(file), file.write)
-
-        ftp.quit()
-
     def _connect_ftp(self, stage):
         """
         connect to the vendor's ftp, and set the cwd according to the stage
@@ -59,6 +30,48 @@ class FileTransmitter:
                 raise Exception('Wrong Stage')
 
         ftp = FTP(host)
+        print("{}'s FTP connected".format(vendor))
         ftp.login(user=username, passwd=password)
+        print('Login Success\n')
         ftp.cwd(dir)
         return ftp
+
+    def download_job(self, job):
+
+        def ftp_loop(fileList, jobPath):
+            """
+            nested loop function within download_job() to work with folder tree
+            """
+            print('ftp_loop running...\n')
+            for file, data in fileList:
+                fileType = data['type']
+
+                if fileType == 'dir':
+                    print('{} is a directory'.format(file))
+                    jobPath = jobPath / file
+                    jobPath.mkdir(exist_ok=True)
+                    ftp.cwd(file)
+                    print('cd to {}'.format(file))
+                    fileList = ftp.mlsd()
+                    ftp_loop(fileList, jobPath) #re-run this function with the subfolders
+
+                elif fileType == 'file':
+                    filePath = jobPath / file
+                    with open(filePath, 'wb') as f:
+                        print('{} is being downloaded'.format(file))
+                        ftp.retrbinary('RETR {}'.format(file), f.write)
+                        print('{} is downloaded'.format(file))
+
+        ftp = self._connect_ftp('download')
+        ftp.cwd(job)
+
+        fileList = ftp.mlsd() #return tuple (filename, data dict)
+
+        jobPath = FileTransmitter.TBQ / job
+        jobPath.mkdir(exist_ok=True)
+
+        ftp_loop(fileList, jobPath)
+        ftp.quit()
+
+    def upload_job(self, job):
+        pass
