7184b24d6f661d7890b335d23c7dc112a651a5c3
zeric <zeric0212@hotmail.com>
2022-11-03 00:53:21 +0800

    initiating

--- pyhs/JobStatusReport.py
+++ pyhs/JobStatusReport.py
@@ -2,6 +2,19 @@ from importlib import resources
 import gspread
 import json
 
-class JSR:
+class JobStatusReport:
+    with resources.open_text('Resources', 'service_account.json') as credJSON:
+        #convert to dict type as gspread.auth.service_account accepts only filename path or dict
+        credDict = json.load(credJSON)
+    gc = gspread.service_account_from_dict(credDict)
+
     def __init__(self, vendor):
+        try:
+            with resources.open_text('Resources', 'JobStatusReport.json') as jsrJSON:
+                self.vendorJsrJSON = json.load(jsrJSON)[vendor]
+                self.vendorJSR = JobStatusReport.gc.open(self.vendorJsrJSON)
+        except KeyError:
+            print(f'No Job Status Report for {vendor}')
+            self.vendorJSR = None
+            return self.vendorJSR
 
